31aabd8881eaff7150e72c491b7f6c8bee9abdc3
SwapneelM <swapneel.mehta96@gmail.com>
2018-03-15 17:44:39 +0100

    modified script to set environment variables

--- lxplus_env.sh
+++ lxplus_env.sh
@@ -1,9 +1,9 @@
-
-export DEEPJETCORE=../DeepJetCore
-
-THISDIR=`pwd`
-cd $DEEPJETCORE/
-source lxplus_env.sh
-cd $THISDIR
-export PYTHONPATH=`pwd`/modules:$PYTHONPATH
 export DEEPJET=`pwd`
+
+export PYTHONPATH=$DEEPJET/modules:$PYTHONPATH
+
+export DEEPJETCORE=$CONDA_PREFIX/python2.7/site-packages/DeepJetCore
+export LD_LIBRARY_PATH=$DEEPJETCORE/compiled:$LD_LIBRARY_PATH
+export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so
+
+export PATH=$DEEPJETCORE/bin:$PATH

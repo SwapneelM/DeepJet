e854b18086f187bb75bdd49a0a6d546aa9a0923d
SwapneelM <swapneel.mehta96@gmail.com>
2018-06-13 16:11:53 +0200

    Removed redundant environment variables Eliminates the dependency for DeepJet (or any other package) and DeepJetCore to lie in the same parent directory.

--- lxplus_env.sh
+++ lxplus_env.sh
@@ -1,9 +1,9 @@
 
-export DEEPJETCORE=../DeepJetCore
+# export DEEPJETCORE=../DeepJetCore
 
-THISDIR=`pwd`
-cd $DEEPJETCORE/
-source lxplus_env.sh
-cd $THISDIR
+# THISDIR=`pwd`
+# cd $DEEPJETCORE/
+# source lxplus_env.sh
+# cd $THISDIR
 export PYTHONPATH=`pwd`/modules:$PYTHONPATH
-export DEEPJET=`pwd`
+# export DEEPJET=`pwd`

7e8bad88371683f4a6a6bc4759128f8998cbc829
SwapneelM <swapneel.mehta96@gmail.com>
2018-07-31 14:23:47 +0200

    Modified environment script

--- pypkg_env.sh
+++ pypkg_env.sh
@@ -1,13 +1,14 @@
 
 # export DEEPJETCORE=../DeepJetCore/DeepJetCore
+conda activate deepjetpkg
 
 THISDIR=`pwd`
 # cd $DEEPJETCORE/
-# source lxplus_env.sh
 # cd $THISDIR
 export PYTHONPATH=`pwd`/modules:$PYTHONPATH
 # export DEEPJET=`pwd`
 export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so
+# export LD_PRELOAD=$CONDA_PREFIX/lib/libCore.so:$LD_PRELOAD
 
 # This path must be set manually for now but will be automated in the future
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:`pwd`/../DeepJetCore/DeepJetCore/compiled

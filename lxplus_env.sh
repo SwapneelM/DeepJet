0e9e630492a3be36cfe075bb639d21bb5cf74d5b
Swapneel Mehta <swapneel.mehta@djsce.edu.in>
2019-02-04 08:20:39 -0500

    Remove unnecessary variables in lxplus_env

--- lxplus_env.sh
+++ lxplus_env.sh
@@ -1,3 +1,5 @@
+'''
+# Python-package original structure
 export DEEPJET=`pwd`  # modify to something akin to os.path.dirname(os.path.abspath(__file__))
 
 export PYTHONPATH=$DEEPJET/modules:$PYTHONPATH
@@ -11,3 +13,12 @@ export LD_LIBRARY_PATH=$DEEPJETCORE/compiled:$LD_LIBRARY_PATH
 export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so
 
 export PATH=$DEEPJETCORE/bin:$PATH
+'''
+#export DEEPJETCORE=../DeepJetCore
+
+#THISDIR=`pwd`
+#cd $DEEPJETCORE/
+#source lxplus_env.sh
+#cd $THISDIR
+export PYTHONPATH=`pwd`/modules:$PYTHONPATH
+#export DEEPJET=`pwd`

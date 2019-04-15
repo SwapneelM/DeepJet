099a2519f82d478aa2c58f6af52b283ba58ffe97
Swapneel Mehta <swapneel.mehta96@gmail.com>
2019-04-15 17:40:48 +0200

    Delete comments

--- pypkg_env.sh
+++ pypkg_env.sh
@@ -1,15 +1,9 @@
-
-# export DEEPJETCORE=../DeepJetCore/DeepJetCore
 conda activate deepjetpkg
 
 THISDIR=`pwd`
-# cd $DEEPJETCORE/
-# cd $THISDIR
 export PYTHONPATH=`pwd`/modules:$CONDA_PREFIX/lib:$PYTHONPATH
-# export DEEPJET=`pwd`
-export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so
-# export LD_PRELOAD=$CONDA_PREFIX/lib/libCore.so:$LD_PRELOAD
+export LD_PRELOAD=$CONDA_PREFIX/lib/libmkl_core.so:$CONDA_PREFIX/lib/libmkl_sequential.so:$LD_PRELOAD
 
 # This path must be set manually for now but will be automated in the future
-export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:`pwd`/../DeepJetCore/DeepJetCore/compiled
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:`pwd`/../DeepJetCore/DeepJetCore/compiled:$LD_LIBRARY_PATH
 export PATH=$CONDA_PREFIX/lib:$PATH
